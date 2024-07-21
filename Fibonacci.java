377014e1209865f8353183c0cf3d975a351ce604
Arnav Saxena <saxenaarnav2001@gmail.com>
2024-07-21 21:05:30 +0530

    Update Fibonacci.java
    unfinished code fix

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the amount of fibonacci you want to see");
-        int a = in.nextInt();
-        for(int i =0 ; i <= a ; i++){
-           int b
+        System.out.print("Enter the amount of fibonacci you want to see: ");
+        int x = Integer.parseInt(in.nextLine());
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        while (c <= x){
+            int temp = b;
+             b = a + b;
+             a = temp;
+             c++;
         }
+        System.out.println(b);
     }
 }
